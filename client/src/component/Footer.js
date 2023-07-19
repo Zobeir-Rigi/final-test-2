cfc7ae74cbd3d861cfe537a361856a2466afd975
zobeir-rigi <rigi.zobir@yahoo.com>
2023-07-19 23:41:09 +0100

    Link Added

--- client/src/component/Footer.js
+++ client/src/component/Footer.js
@@ -1,5 +1,6 @@
+import { Link } from "react-router-dom";
 export const Footer = () => {
     return (
-       <> Created by Zobeir Rigi </>
+       <> Created by <Link to ="https://github.com/Zobeir-Rigi">Zobeir Rigi</Link> </>
     );
 };
