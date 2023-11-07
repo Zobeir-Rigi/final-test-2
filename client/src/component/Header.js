acbf3a48384036d3baeb7803fc2ede2753f3cf21
zobeir-rigi <rigi.zobir@yahoo.com>
2023-11-07 15:33:22 +0000

    logo in the left

--- client/src/component/Header.js
+++ client/src/component/Header.js
@@ -4,11 +4,6 @@ import imageSrc from "../../data/imge/cyf-logo.png";
 export const Header = () => {
 	return (
 		<header>
-
-			<p className="topic">
-				<span className="cyf">CYF</span>{" "}
-				<span className="progress">progress tracker</span>
-			</p>
 			<Link to={"/"}>
 				<img
 					className="logo"
@@ -16,6 +11,10 @@ export const Header = () => {
 					alt="CYF"
 				/>
 			</Link>
+			<p className="topic">
+				<span className="cyf">CYF</span>{" "}
+				<span className="progress">progress tracker</span>
+			</p>
 			<p className="team-name">Ctrl + Shift + Learn</p>
 		</header>
 	);
