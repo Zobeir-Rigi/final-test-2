ffee99a4d2ca58a756b554a5b08ff44e99be898a
zobeir-rigi <rigi.zobir@yahoo.com>
2024-01-18 17:05:01 +0000

    we dont need the comments, because we push them into github

--- client/src/AppContext.js
+++ client/src/AppContext.js
@@ -1,6 +1,5 @@
 import { createContext } from "react";
 export const AppContext = createContext({
-	// these values are good for autocompletion
 	userName: "",
 	setUserName: () => {},
 	codewarsData: [],
